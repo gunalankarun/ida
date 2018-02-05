4fde21bbcf8fbe45876f23aa0f9428ddff733f00
Gunalan Karun <gunalan.karun@gmail.com>
2018-02-05 16:39:21 -0600

    Updated view controllers for pages

--- IdaDashboard/DashboardViewController.swift
+++ IdaDashboard/DashboardViewController.swift
@@ -2,7 +2,7 @@
 //  DashboardViewController.swift
 //  IdaDashboard
 //
-//  Created by Gunalan Karun on 1/30/18.
+//  Created by Gunalan Karun on 2/5/18.
 //  Copyright © 2018 Ida. All rights reserved.
 //
 
@@ -12,14 +12,24 @@ class DashboardViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
