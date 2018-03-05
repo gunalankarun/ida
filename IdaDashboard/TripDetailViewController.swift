d413a3eb16b2ac3a30ef86aff6d454f0808feffa
Gunalan Karun <gunalan.karun@gmail.com>
2018-03-05 11:15:31 -0600

    Added CocoaPods integration and Charts library

--- IdaDashboard/TripDetailViewController.swift
+++ IdaDashboard/TripDetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Charts
 
 class TripDetailViewController: UIViewController {
 
---
+++ Pods/Target Support Files/Pods-IdaDashboard/Pods-IdaDashboard-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IdaDashboard : NSObject
+@end
+@implementation PodsDummy_Pods_IdaDashboard
+@end
