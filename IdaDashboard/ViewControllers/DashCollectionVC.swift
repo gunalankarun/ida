406a5aaa643e49218269019b4ac55755d036d44c
Gunalan Karun <gunalan.karun@gmail.com>
2018-03-27 11:50:07 -0500

    Added New Dashboard, added dash image assets, created view for data points

--- IdaDashboard/ViewControllers/DashCollectionVC.swift
+++ IdaDashboard/ViewControllers/DashCollectionVC.swift
@@ -10,7 +10,13 @@ import UIKit
 
 class DashCollectionVC: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     let cellIdentifiers = ["ScoreCell","DashCell1","DashCell2","DashCell3","DashCell4","StartTripCell"]
-    let cellSizes = [CGSize(width:373, height:215),CGSize(width:185, height:180),CGSize(width:185, height:180),CGSize(width:185, height:180),CGSize(width:185, height:180),CGSize(width:369, height:69)]
+    let cellSizes = [
+        CGSize(width:375, height:205),
+        CGSize(width:188, height:175),
+        CGSize(width:187, height:175),
+        CGSize(width:188, height:175),
+        CGSize(width:187, height:175),
+        CGSize(width:369, height:65)]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,13 +25,30 @@ class DashCollectionVC: UICollectionViewController, UICollectionViewDelegateFlow
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return cellIdentifiers.count
     }
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifiers[indexPath.item], for: indexPath)
+        if (indexPath.item < 5) {
+            cell.contentView.layer.borderColor = UIColor(red:0.75, green:0.75, blue:0.75, alpha:1.0).cgColor
+            cell.contentView.layer.borderWidth = 0.5
+        }
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return cellSizes[indexPath.item]
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.layer.backgroundColor = UIColor(red:0.92, green:0.92, blue:0.92, alpha:1.0).cgColor
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.layer.backgroundColor = UIColor.white.cgColor
+    }
+
+
 
 }
